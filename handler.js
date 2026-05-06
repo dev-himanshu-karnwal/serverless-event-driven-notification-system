99f3e61edb52d79a583b92809d16e1a5dfc1c707
Himanshu Karnwal <himanshukar1810@gmail.com>
2026-05-06 17:37:35 +0530

    type module

--- handler.js
+++ handler.js
@@ -1,6 +1,9 @@
-exports.hello = async (event) => {
+export const hello = async (event) => {
   return {
     statusCode: 200,
+    headers: {
+      'Content-Type': 'application/json',
+    },
     body: JSON.stringify({
       message: "Go Serverless v1.0! Your function executed successfully!",
     }),
