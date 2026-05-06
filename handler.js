aecacde384371177954b777d9803edf710e4f2de
Himanshu Karnwal <himanshukar1810@gmail.com>
2026-05-06 17:12:26 +0530

    first commit

---
+++ handler.js
@@ -0,0 +1,8 @@
+exports.hello = async (event) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: "Go Serverless v1.0! Your function executed successfully!",
+    }),
+  };
+};
